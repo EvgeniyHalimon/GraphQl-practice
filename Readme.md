6cc38c88adfdb4a6664dacc31b376806d00f081c
EvgeniyHalimon <halimonevgeniy@gmail.com>
2023-05-05 11:22:55 +0300

    first commit

---
+++ Readme.md
@@ -0,0 +1,6 @@
+## GraphQl practice
+### Tech stack
+`apollo-server`
+`graphql`
+`prisma`
+`SQLite`
---
+++ src/resolvers/Vote.js
@@ -0,0 +1,7 @@
+export function voteLink(parent, args, context) {
+    return context.prisma.vote.findUnique({ where: { id: parent.id } }).link()
+}
+  
+export function voteUser(parent, args, context) {
+  return context.prisma.vote.findUnique({ where: { id: parent.id } }).user()
+}
